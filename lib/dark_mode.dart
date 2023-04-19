3049bc17e4a289ee55a47e1954d6a417efcf626a
DavidPerezContreras <david.perez.dam2@gmail.com>
2023-04-19 17:05:53 +0200

    onSwitch removed (unused)

--- lib/dark_mode.dart
+++ lib/dark_mode.dart
@@ -2,11 +2,4 @@ import 'package:flutter/material.dart';
 
 class DarkMode extends ValueNotifier{
   DarkMode(super.value);
-  
-
-
-  onSwitch(bool value){
-    value=value;
-    notifyListeners();
-  }
 }
